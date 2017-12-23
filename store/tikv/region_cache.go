c6b27193bf7f1aa43ec07c11352e40b355eea56b
sunjun <admin@sunsblog.cn>
2017-12-24 01:30:32 +0800

    1

--- store/tikv/region_cache.go
+++ store/tikv/region_cache.go
@@ -281,15 +281,7 @@ func (c *RegionCache) getCachedRegion(id RegionVerID) *Region {
 	}
 	if cachedRegion.isValid() {
 		//atomic.StoreInt64(&cachedRegion.lastAccess, time.Now().Unix())
-<<<<<<< HEAD
-<<<<<<< HEAD
-		cachedRegion.lastAccess=time.Now().Unix()
-=======
 		cachedRegion.lastAccess = time.Now().Unix()
->>>>>>> master
-=======
-		cachedRegion.lastAccess = time.Now().Unix()
->>>>>>> master
 		return cachedRegion.region
 	}
 	return nil
